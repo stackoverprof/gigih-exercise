8b12f09ec1ceb0436189a016bf843d9fe249f7a2
stackoverprof <mail.errbint@gmail.com>
2022-03-23 19:05:48 +0700

    feat: exercise 23 mar #2

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,6 +10,7 @@ const Home = () => {
     url: "https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/200w.gif?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=200w.gif&ct=g",
     webp: "https://media4.giphy.com/media/4HrBfVJJveBNS9ytSk/giphy.webp?cid=cb3f2bebpuo6jj0g5f9gfibjre2zzbb4yb1cfshtplanlrpw&rid=giphy.webp&ct=g",
   };
+
   return (
     <header className="App-header">
       <Form />
