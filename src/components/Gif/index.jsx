13e39587b256cce667f8e3746525b533f2164133
stackoverprof <mail.errbint@gmail.com>
2022-03-28 20:17:43 +0700

    feat: gigih exer mod 3 ses 1

--- src/components/Gif/index.jsx
+++ src/components/Gif/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Gif = ({ url, title }) => {
+  console.log(title, url);
   return <img src={url} alt={title} className="image-gif" />;
 };
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,55 @@
-import React from "react";
-import Form from "../../components/Form";
+import React, { Component } from "react";
+import SearchBox from "../../components/SearchBox";
 import Gif from "../../components/Gif";
-import DATA_GIFS from "../../data/gifs";
+import axios from "axios";
 
-const Home = () => {
-  const data = DATA_GIFS;
-  const filtered_data = data.filter((item) => item.rating === "g");
-  return (
-    <header className="App-header">
-      <Form />
-      {filtered_data.map((item, i) => (
-        <Gif title={item.title} url={item.url} key={i} />
-      ))}
-    </header>
-  );
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResult: [],
+      searchQuery: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const params = {
+      api_key: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+      q: this.state.searchQuery,
+      limit: 15,
+    };
+    const result = await axios
+      .get("https://api.giphy.com/v1/gifs/search", {
+        params,
+      })
+      .then((res) => res.data.data)
+      .catch((err) => console.error(err));
+    this.setState({ searchResult: result });
+  };
+
+  render() {
+    return (
+      <header className="App-header">
+        <SearchBox
+          value={this.state.searchQuery}
+          onChange={this.onChange}
+          onSubmit={this.onSubmit}
+        />
+        <div className="gifs-area">
+          {this.state.searchResult.map((item, i) => (
+            <Gif title={item.title} url={item.images.fixed_width.url} key={i} />
+          ))}
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Home;
