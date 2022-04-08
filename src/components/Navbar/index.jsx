73c28f1cbac248a3c14416a554bd329c78312ca9
stackoverprof <mail.errbint@gmail.com>
2022-04-08 07:30:43 +0700

    fix: convert to func home

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,10 +11,16 @@ const Navbar = () => {
           <h2 className="text-[#fff5] text-2xl">Giphy Client</h2>
         </div>
         <div className="flex-cc gap-4">
-          <Link to="/" className="block px-3 py-2 rounded-md border">
+          <Link
+            to="/"
+            className="block px-3 hover:bg-[#fff3] py-2 rounded-md border"
+          >
             Home
           </Link>
-          <Link to="/trending" className="block px-3 py-2 rounded-md border">
+          <Link
+            to="/trending"
+            className="block px-3 hover:bg-[#fff3] py-2 rounded-md border"
+          >
             Trending
           </Link>
         </div>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import SearchBar from "../../components/SearchBar";
 import Gif from "../../components/Gif";
 import axios from "axios";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchResult: [],
-      searchQuery: "",
-    };
-  }
-
-  onChange = (e) => {
-    this.setState({ searchQuery: e.target.value });
+const Home = () => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const onChange = (e) => {
+    setSearchQuery(e.target.value);
   };
 
-  onSubmit = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     const params = {
       api_key: process.env.REACT_APP_GIPHY_CLIENT_ID,
-      q: this.state.searchQuery,
+      q: searchQuery,
       limit: 15,
     };
 
@@ -33,25 +26,19 @@ class Home extends Component {
       .then((res) => res.data.data)
       .catch((err) => console.error(err));
 
-    this.setState({ searchResult: result });
+    setSearchResult(result);
   };
 
-  render() {
-    return (
-      <header className="App-header">
-        <SearchBar
-          value={this.state.searchQuery}
-          onChange={this.onChange}
-          onSubmit={this.onSubmit}
-        />
-        <div className="container grid grid-cols-3 gap-4">
-          {this.state.searchResult.map((item, i) => (
-            <Gif title={item.title} url={item.images.fixed_width.url} key={i} />
-          ))}
-        </div>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="App-header">
+      <SearchBar value={searchQuery} onChange={onChange} onSubmit={onSubmit} />
+      <div className="container grid grid-cols-3 gap-4">
+        {searchResult.map((item, i) => (
+          <Gif title={item.title} url={item.images.fixed_width.url} key={i} />
+        ))}
+      </div>
+    </header>
+  );
+};
 
 export default Home;
