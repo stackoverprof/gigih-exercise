066bee0b8037507e3c21ba0543a35b6a63fd2ec3
stackoverprof <mail.errbint@gmail.com>
2022-04-08 07:46:00 +0700

    feat: styling search button

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -8,10 +8,13 @@ export default class SearchBar extends Component {
           value={this.props.value}
           onChange={this.props.onChange}
           type="text"
-          className="search-input"
+          className="search-input h-16"
           placeholder="Search something..."
         />
-        <button type="submit" className="search-button">
+        <button
+          type="submit"
+          className="h-16 ml-4 bg-white bg-opacity-10 px-8 rounded-md font-semibold text-2xl"
+        >
           SEARCH!
         </button>
       </form>
