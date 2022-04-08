8c43de398a47a22b639cf9b2fdcc6695d5141a0e
stackoverprof <mail.errbint@gmail.com>
2022-04-08 07:41:43 +0700

    feat: gif hover effect

--- src/components/Gif/index.jsx
+++ src/components/Gif/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Gif = ({ url, title }) => {
-  console.log(title, url);
-  return <img src={url} alt={title} className="full object-cover rounded-lg" />;
+  return (
+    <div className="full flex-cc rounded-lg overflow-hidden relative">
+      <img src={url} alt={title} className="full object-cover" />
+      <div className="absolute p-6 hover:opacity-100 opacity-0 full top-0 left-0 flex-cc bg-black bg-opacity-60">
+        <p className="text-3xl">{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Gif;
